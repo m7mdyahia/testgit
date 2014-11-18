b30be0d7ee9abdc4a1c8a0b93bd9f2ac2bc8b482
Mohammad Yahia <moda.yahia@gmail.com>
2014-11-18 16:58:24 +0200

    tgf

--- testgit/gen/com/example/testgit/R.java
+++ testgit/gen/com/example/testgit/R.java
@@ -14,7 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class string {
+        public static final int action_settings=0x7f030003;
         public static final int app_name=0x7f030000;
+        public static final int hello_world=0x7f030002;
+        public static final int title_activity_main=0x7f030001;
     }
     public static final class style {
         /** 
